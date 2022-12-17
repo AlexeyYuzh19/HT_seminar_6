df1acc6ad67ad09907912790ef8508eac5656aba
AlexeyYuzh19 <9676331836@mail.ru>
2022-12-17 19:17:40 +0500

    подготовка решения задачи 41

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -6,4 +6,22 @@
 
 Console.WriteLine("Hello, World!");
 
+/*   Вариант введения массива в одну строку, но в диапазоне от 0 до 9
 
+int i;
+int[] a = new int[8];
+Console.WriteLine("Введите массив чисел: ");
+for (i = 0; i < 8; i++)
+{
+a[i] = Console.ReadKey().KeyChar - '0';
+Console.Write(", ");
+}
+System.Console.WriteLine("\n[ " + string.Join(" , ", a) + " ]");
+*/
+
+/*  Вариант введения массива в одну строку с разделением значений запятой:
+
+int[] ar = Console.ReadLine().Split(new char[]{','}, 
+StringSplitOptions.RemoveEmptyEntries).Select(i=>int.Parse(i)).ToArray<int>();
+System.Console.WriteLine("\n[ " + string.Join(" , ", ar) + " ]");
+*/
