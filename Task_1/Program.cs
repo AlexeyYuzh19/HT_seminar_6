152d12af339038ce08c4c3bd4f3128b0f15a8b24
AlexeyYuzh19 <9676331836@mail.ru>
2022-12-18 20:11:16 +0500

    код задачи 41 с методами и переборкой массива циклом foreach

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -29,23 +29,44 @@ int CheckInputNumber(string Text)
     return number;
 }
 
-int[] Array()
+int[] ArCopy(int[] array, int Length)
 {
-    int M = CheckInputNumber("Задайте количество чисел для ввода : ");
+    int[] Copy = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        Copy[i] = array[i];
+    }
+    return Copy;
+}
 
+int[] MadeArray()
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    System.Console.WriteLine("\nВводите числа до заполнения массива, а если утомитесь - жмите спасительный код - 011 !\n");
+
+    const int M = 1000;
+    int count = 0;
     int[] arr = new int[M];
 
     for (int i = 0; i < M; i++)
     {
         arr[i] = CheckInputNumber($"Введите {i + 1}-й элемент : ");
+        if (arr[i] == 011 || i == 999) break;
+        count++;
     }
-    return arr;
+
+    int[] arrCopy = ArCopy(arr, count);
+
+    return arrCopy;
 }
 
-void NumMoreNull(int[] array, out int Numbers)
+void NumMoreNull(int[] array, out int Numbers, out int arL)
 {
     Numbers = 0;
+    arL = array.Length;
 
+    //for (int i = 0; i < array.Length; i++)
     foreach (int ari in array)
     {
         if (ari > 0)
@@ -66,12 +87,12 @@ void NumMoreNull(int[] array, out int Numbers)
     }
 }
 
-int[] array = Array();
+int[] array = MadeArray();
 
 System.Console.WriteLine("\nВведены следующие значения чисел : \n");
 
-NumMoreNull(array, out int Numbers);
+NumMoreNull(array, out int Numbers, out int arL);
 
 Console.ForegroundColor = ConsoleColor.Red;
-System.Console.WriteLine($"\n\nB данном массиве чисел количество значений больше нуля = {Numbers}\n");
+System.Console.WriteLine($"\n\nB данном массиве из {arL} чисел количество значений больше нуля = {Numbers}\n");
 Console.ResetColor();
